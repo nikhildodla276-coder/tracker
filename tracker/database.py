5df4502ea9c055021c3eda3814cb1c44b973f628
Dodla Nikhil <nikhildodla276@gmail.com>
2026-05-17 15:19:35 +0000

    feat: add database.py with SQLite connection and logs table

---
+++ tracker/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def get_db_connection():
+    conn= sqlite3.connect("tracker.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS logs (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   date TEXT NOT NULL,
+                   task_name TEXT NOT NULL,
+                   rating INTEGER NOT NULL,
+                   notes TEXT
+                   )
+                """)
+    conn.commit()
+    conn.close
+
+if __name__ == "__main__":
+    init_db()
+    print("Database initialized.")
